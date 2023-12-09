bbd5249655f7c2e80829babd0a393ea46ab91cbf
Mara <mara-li@outlook.fr>
2023-12-09 12:24:00 +0100

    Add join event handler

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as process from "process";
 import * as pkg from "../package.json";
 import interaction from "./events/interaction";
 import ready from "./events/ready";
-
+import join from "./events/join";
 dotenv.config({ path: ".env" });
 
 export const client = new Client({
@@ -29,6 +29,7 @@ export const prod = process.env.NODE_ENV === "production";
 try {
 	ready(client);
 	interaction(client);
+	join(client);
 }
 catch (error) {
 	console.error(error);
