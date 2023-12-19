499dc15c4e58c17f6fad085ea5ed395c208984e1
Mara-Li <lili.simonetti@outlook.fr>
2023-12-19 09:51:16 +0100

    feat: allow to get the comments from a simple message after dice roll (without comments notation)

--- src/events/message_create.ts
+++ src/events/message_create.ts
@@ -1,16 +1,20 @@
 import {ChannelType, Client, ForumChannel, TextChannel, ThreadChannel, userMention} from "discord.js";
+import moment from "moment";
+import { Resultat } from "src/interface";
+
 import { deleteAfter } from "../commands";
-import { Parser } from "@dice-roller/rpg-dice-roller";
+import { COMMENT_REGEX, parseResult, roll } from "../dice";
 import en from "../locales/en";
 import fr from "../locales/fr";
-import { COMMENT_REGEX, parseResult, roll } from "../dice";
-import moment from "moment";
 import { findForumChannel, findThread } from "../utils";
-import { Resultat } from "src/interface";
+
 const TRANSLATION = {
 	fr,
 	en
-}
+};
+
+const DETECT_DICE_MESSAGE = /([\w\.]+|(\{.*\})) (.*)/;
+
 export default (client: Client): void => {
 	client.on("messageCreate", async (message) => {
 		if (message.author.bot) return;
@@ -18,7 +22,11 @@ export default (client: Client): void => {
 		if (!message.guild) return;
 		let content = message.content;
 		//detect roll between bracket
-		const detectRoll = content.match(/\[(.*)\]/)?.[1]
+		const detectRoll = content.match(/\[(.*)\]/)?.[1];
+		const rollWithMessage = content.match(DETECT_DICE_MESSAGE)?.[3];
+		if (rollWithMessage) {
+			content = content.replace(DETECT_DICE_MESSAGE, "$1 /* $3 */");
+		}
 		let deleteInput=true;
 		let result: Resultat| undefined;
 		try {
@@ -26,25 +34,24 @@ export default (client: Client): void => {
 		} catch(e) {
 			return;
 		}
-		if (detectRoll) {
-			content = detectRoll;
+		if (detectRoll || rollWithMessage) {
 			deleteInput = false;
 		}
 		//is a valid roll as we are in the function so we can work as always
-		const userLang = message.guild.preferredLocale ?? "en"
+		const userLang = message.guild.preferredLocale ?? "en";
 		const translation = TRANSLATION[userLang as keyof typeof TRANSLATION] || TRANSLATION.en;
 		const channel = message.channel;
 		if (!result) return;
 		const parser = parseResult(result);
 
 		if (channel instanceof TextChannel && channel.name.startsWith("🎲")) {
-			await message.reply({content: parser});
+			await message.reply({content: parser, allowedMentions: { repliedUser: false }});
 			return;
 		}
 		if (channel instanceof TextChannel || (channel.parent instanceof ForumChannel && !channel.name.startsWith("🎲"))) {
 			let linkToOriginal = "";
 			if (deleteInput) {
-				message.delete()
+				message.delete();
 			} else {
 				linkToOriginal = `\n\n__Original__: ${message.url}`;
 			}
@@ -54,10 +61,12 @@ export default (client: Client): void => {
 			const msg = `${authorMention} - <t:${moment().unix()}>\n${parser}${linkToOriginal}`;
 			await msgToEdit.edit(msg);
 			const idMessage = `↪ ${msgToEdit.url}`;
-			const reply = deleteInput ? await channel.send({ content: `${authorMention}\n${parser}\n\n${idMessage}` }) : await message.reply({ content: `${parser}\n\n${idMessage}` });
-			deleteAfter(reply, 180000)
+			const reply = deleteInput ?
+				await channel.send({ content: `${authorMention}\n${parser}\n\n${idMessage}` })
+				: await message.reply({ content: `${parser}\n\n${idMessage}` , allowedMentions: { repliedUser: false }});
+			deleteAfter(reply, 180000);
 			return;
 		}
-		await message.reply({content: parser});
-	})
-}
+		await message.reply({content: parser, allowedMentions: { repliedUser: false }});
+	});
+};
