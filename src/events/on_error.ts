6a9d3da9ecba7e28a34f63fef1567ee01c6d9cea
Mara <mara-li@outlook.fr>
2024-03-09 13:32:56 +0100

    send an message to the owner (me) when error occur

--- src/events/on_error.ts
+++ src/events/on_error.ts
@@ -1,8 +1,15 @@
 import { Client } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
 
 export const botError = (client	: Client): void => {
-	client.on("error", (error) => {
+	client.on("error", async (error) => {
 		console.error(error);	//prevent the crash of the entire application
+		//send a message to the owner of the bot
+		if (!process.env.OWNER_ID) return;
+		const dm = await client.users.createDM(process.env.OWNER_ID);
+		dm.send(`An error has occurred:\n\`\`\`\n${error}\n\`\`\``);
 	});
 };
 
