676e286969132d5d7b3dcb7f1125f00278a67cc5
Mara <mara-li@outlook.fr>
2024-03-22 17:43:20 +0100

    Update French localization and user buttons

--- src/utils/buttons.ts
+++ src/utils/buttons.ts
@@ -5,26 +5,31 @@ const addTemplateButton = (ul: TFunction<"translation", undefined>) => {
 	return new ButtonBuilder()
 		.setCustomId("add_template")
 		.setLabel(ul("button.add.template"))
+		.setEmoji("➕")
 		.setStyle(ButtonStyle.Primary);
 };
 
 export function editUserButtons(ul: TFunction<"translation", undefined>, stats?: boolean, dice?: boolean, template?: boolean) {
-	const editUser = new ButtonBuilder()
-		.setCustomId("edit_stats")
-		.setLabel(ul("button.edit.stats"))
-		.setStyle(ButtonStyle.Primary);
-	const editDice = new ButtonBuilder()
-		.setCustomId("edit_dice")
-		.setLabel(ul("button.edit.dice"))
-		.setStyle(ButtonStyle.Primary);
 	const addDice = new ButtonBuilder()
 		.setCustomId("add_dice")
 		.setLabel(ul("button.dice"))
+		.setEmoji("➕")	
 		.setStyle(ButtonStyle.Primary);
+	const editUser = new ButtonBuilder()
+		.setCustomId("edit_stats")
+		.setLabel(ul("button.edit.stats"))
+		.setEmoji("📝")
+		.setStyle(ButtonStyle.Secondary);
+	const editDice = new ButtonBuilder()
+		.setCustomId("edit_dice")
+		.setLabel(ul("button.edit.dice"))
+		.setEmoji("📝")
+		.setStyle(ButtonStyle.Secondary);
 	const editTemplate = new ButtonBuilder()
 		.setCustomId("edit_template")
 		.setLabel(ul("button.edit.template"))
-		.setStyle(ButtonStyle.Primary);
+		.setStyle(ButtonStyle.Secondary)
+		.setEmoji("📝");
 	if (stats && dice && template)	
 		return new ActionRowBuilder<ButtonBuilder>().addComponents([editUser, editDice, addDice, editTemplate]);
 	const components = [addDice];
