11582a0c2e7628675fcc7df60c04bc5f1472aa7b
Mara <mara-li@outlook.fr>
2024-04-14 15:55:16 +0200

    fix error

--- src/events/on_error.ts
+++ src/events/on_error.ts
@@ -1,3 +1,4 @@
+import {error as err} from "@console";
 import { EClient } from "@main";
 import dotenv from "dotenv";
 
@@ -5,7 +6,7 @@ dotenv.config({ path: ".env" });
 
 export const botError = (client	: EClient): void => {
 	client.on("error", async (error) => {
-		error(error);
+		err(error);
 		if (!process.env.OWNER_ID) return;
 		const dm = await client.users.createDM(process.env.OWNER_ID);
 		dm.send(`An error has occurred:\n\`\`\`\n${error.message}\n\`\`\``);
