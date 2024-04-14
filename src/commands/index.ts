5fb0946a054f72c1df56962ee180008ecfd6ecfa
Mara <mara-li@outlook.fr>
2024-04-14 22:18:30 +0200

    forgot help

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,9 @@
 import { admin } from "./admin";
 import { deleteChar } from "./admin/delete_char";
 import { gimmick } from "./gimmick";
+import { help } from "./help";
 import { roll_auto, roll_cmdList } from "./rolls";
 
 export const autCompleteCmd = [...roll_auto, ...gimmick, deleteChar];
-export const commandsList = [...roll_auto, ...roll_cmdList, ...gimmick, ...admin, deleteChar
+export const commandsList = [...roll_auto, ...roll_cmdList, ...gimmick, ...admin, deleteChar, help
 ];
