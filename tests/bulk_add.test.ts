18b7ec13a8a4c55451770fadcf089c6a9925e04d
Mara <mara-li@outlook.fr>
2024-05-17 13:19:20 +0200

    move constant/expected result

--- tests/bulk_add.test.ts
+++ tests/bulk_add.test.ts
@@ -1,63 +1,7 @@
-import { StatisticalTemplate } from "@dicelette/core";
 import { readFileSync } from "fs";
 
 import { parseCSV } from "../src/commands/admin/bulk_add";
-import { UserData } from "../src/interface";
-
-const guildTemplate: StatisticalTemplate = {
-	charName: true,
-	statistics: {
-		"STR": { max: 18, min: 3 },
-		"DEX": { max: 18, min: 3 },
-		"CON": { max: 18, min: 3 },
-		"INT": { max: 18, min: 3 },
-		"WIS": { max: 18, min: 3 },
-		"CHA": { max: 18, min: 3 },
-	},
-	total: 88,
-	diceType: "4d6",
-	critical: { success: 20, failure: 1 },
-	damage: {
-		"STR": "1d4",
-		"DEX": "1d4",
-		"CON": "1d4",
-		"INT": "1d4",
-		"WIS": "1d4",
-		"CHA": "1d4",
-	},
-};
-
-const temp = {
-	diceType: "4d6",
-	critical: { success: 20, failure: 1 }
-};
-
-const expectedResult: {[id: string]: UserData[]} = {
-	"mara__li" : [{
-		userName: "Blaïka",
-		stats: {
-			"STR": 12,
-			"DEX": 12,
-			"CON": 12,
-			"INT": 12,
-			"WIS": 12,
-			"CHA": 12,
-		},
-		template: temp,
-	}],
-	"truc" : [{
-		userName: "machin",
-		stats: {
-			"STR": 11,
-			"DEX": 10,
-			"CON": 11,
-			"INT": 10,
-			"WIS": 11,
-			"CHA": 10,
-		},
-		template: temp,
-	}],
-};
+import { expectedResult, guildTemplate } from "./constant.tests";
 
 describe("parseCSV", () => {
 	it("should be valid and equal", async () => {
