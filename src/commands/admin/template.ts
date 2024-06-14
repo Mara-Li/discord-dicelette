a9e5125004df9d579311602fed6446ced9d4111d
Mara <mara-li@outlook.fr>
2024-06-14 15:38:07 +0200

    fix: add an error when the file is not a JSON file

--- src/commands/admin/template.ts
+++ src/commands/admin/template.ts
@@ -173,6 +173,10 @@ export const registerTemplate = {
 		const ul = ln(interaction.locale as Locale);
 		const template = options.getAttachment(t("register.options.template.name"), true);
 		//fetch the template
+		if (!template.contentType?.includes("json")) {
+			await reply(interaction, { content: ul("error.template"), ephemeral: true });
+			return;
+		}
 		const res = await fetch(template.url).then(res => res.json());
 		const templateData = verifyTemplateValue(res);
 		const guildId = interaction.guild.id;
