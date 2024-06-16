df33a8bef1fbbf076f50223fbc2ce70aa04bc8f9
Mara <mara-li@outlook.fr>
2024-06-16 13:07:34 +0200

    trying to fix conversion in vps

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,6 @@
 import { commandsList } from "@commands";
 import { log, success } from "@console";
-import type { GuildData, Settings } from "@interface";
+import type { Settings } from "@interface";
 import { type EClient, VERSION } from "@main";
 import { ActivityType, type Guild, REST, Routes } from "discord.js";
 import dotenv from "dotenv";
@@ -18,7 +18,6 @@ export default (client: EClient): void => {
 		}
 		success(`${client.user.username} is online; v.${VERSION}`);
 		const serializedCommands = commandsList.map((command) => command.data.toJSON());
-		const wasconverted = convertJSONToEnmap(client);
 		client.user.setActivity("Roll Dices 🎲 !", { type: ActivityType.Competing });
 		for (const guild of client.guilds.cache.values()) {
 			log(`Registering commands for ${guild.name}`);
@@ -32,37 +31,12 @@ export default (client: EClient): void => {
 			await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guild.id), {
 				body: serializedCommands,
 			});
-			if (wasconverted) {
-				try {
-					const owner = await guild.members.fetch(guild.ownerId);
-					if (owner) {
-						owner.send(
-							`[MESSAGE FOR SERVER: ${guild.name}]\nThe database of Dicelette was updated into a new more secure format!\nNormally, all your data should be working but if you notice any missing data, please contact the bot owner! All old data was moved to a backup file, so don't worry!\n\nHave a nice day :)`
-						);
-					}
-				} catch (e) {
-					//skip
-				}
-			}
+
 			convertDatabaseUser(client.settings, guild);
 		}
 		cleanData(client);
 	});
 };
-
-function convertJSONToEnmap(Client: EClient) {
-	if (!fs.existsSync("database.json")) {
-		return false;
-	}
-	const data = fs.readFileSync("database.json", "utf8");
-	const parsedData = JSON.parse(data) as { [key: string]: GuildData };
-	for (const [guildId, guildData] of Object.entries(parsedData)) {
-		Client.settings.set(guildId, guildData);
-	}
-	//move the file to a backup
-	fs.renameSync("database.json", `database_${Date.now()}.json`);
-	return true;
-}
 
 function convertDatabaseUser(db: Settings, guild: Guild) {
 	if (db.get(guild.id, "converted")) return;
@@ -71,11 +45,16 @@ function convertDatabaseUser(db: Settings, guild: Guild) {
 	const defaultChannel = db.get(guild.id, "managerId");
 	const privateChannel = db.get(guild.id, "privateChannel");
 	for (const [userId, userData] of Object.entries(users)) {
-		log(`Converting ${userId} => ${JSON.stringify(userData)} in ${guild.name}`);
+		console.warn(`Converting ${userId} => ${JSON.stringify(userData)} in ${guild.name}`);
 		for (const index in userData) {
 			const data = userData[index];
 			if (!Array.isArray(data.messageId)) {
-				if (!privateChannel && !defaultChannel) continue;
+				if (!privateChannel || !defaultChannel) {
+					console.error("Missing channel for the conversion");
+					//remove the user
+					db.delete(guild.id, `user.${userId}.${index}`);
+					continue;
+				}
 				if (data.isPrivate && privateChannel)
 					data.messageId = [data.messageId, privateChannel];
 				else if (defaultChannel) data.messageId = [data.messageId, defaultChannel];
