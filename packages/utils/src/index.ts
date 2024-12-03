c269813416157aac6e1814b92010c4132d7c9dcf
Mara <mara-li@outlook.fr>
2024-12-03 18:54:29 +0100

    chore: continue

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,11 @@
+import EnglishUS from "./localizations/locales/en.json" assert { type: "json" };
+import French from "./localizations/locales/fr.json" assert { type: "json" };
+
+export const resources = {
+	en: {
+		translation: EnglishUS,
+	},
+	fr: {
+		translation: French,
+	},
+};
